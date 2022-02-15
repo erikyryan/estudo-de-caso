e2cbaf6fdc8cc5bfff7fd9cde333c9ccfaec1a38
Eriky Ryan <erikyryang@gmail.com>
2022-02-15 13:42:45 -0300

    Endpoint /pedidos/{id}

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -115,8 +115,6 @@ public class CursomcApplication implements CommandLineRunner{
 
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 		
-		
-
 		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
 		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
 		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
@@ -127,8 +125,6 @@ public class CursomcApplication implements CommandLineRunner{
 		p1.getItens().addAll(Arrays.asList(ip1));
 		p2.getItens().addAll(Arrays.asList(ip3));
 		p3.getItens().addAll(Arrays.asList(ip2));
-		
-		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
